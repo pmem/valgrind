17cae0a4c4f68f0b323216a6c083515d0e5267c9
Tomasz Kapela <tomasz.kapela@intel.com>
2015-03-12 16:40:54 +0100

    pmemcheck: initial version of the pmemcheck tool

---
+++ pmemcheck/tests/common.h
@@ -0,0 +1,69 @@
+/*
+ * Persistent memory checker.
+ * Copyright (c) 2014-2015, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, or (at your option) any later version, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/param.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdint.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+#include "pmemcheck/pmemcheck.h"
+
+/**
+* \brief Makes and maps a temporary file.
+* \param[in] size The size of the file.
+*/
+void *
+make_map_tmpfile(size_t size)
+{
+    static char file_path[] = "./pmemcheck.XXXXXX";
+
+    int fd;
+    if ((fd = mkstemp(file_path)) < 0) {
+        return NULL;
+    }
+
+    unlink(file_path);
+
+    if ((errno = posix_fallocate(fd, 0, size)) != 0) {
+        int oerrno = errno;
+        if (fd != -1)
+            close(fd);
+        errno = oerrno;
+        return NULL;
+    }
+
+    void *base;
+    if ((base = mmap(NULL, size, PROT_WRITE, MAP_PRIVATE|MAP_NORESERVE, fd,
+            0)) == MAP_FAILED) {
+        int oerrno = errno;
+        if (fd != -1)
+            close(fd);
+        errno = oerrno;
+        return NULL;
+    }
+
+    close(fd);
+
+    VALGRIND_PMC_REGISTER_PMEM_MAPPING(base, size);
+
+    return base;
+}
---
+++ pmemcheck/tests/logging.c
@@ -0,0 +1,51 @@
+/*
+ * Persistent memory checker.
+ * Copyright (c) 2014-2015, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, or (at your option) any later version, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+#include "common.h"
+
+#define FILE_SIZE (16 * 1024 * 1024)
+
+int main ( void )
+{
+    /* make, map and register a temporary file */
+    void *base = make_map_tmpfile(FILE_SIZE);
+
+    int8_t *i8p = base;
+    int16_t *i16p = base+8;
+    int32_t *i32p = base+16;
+
+    VALGRIND_PMC_LOG_STORES;
+    /* dirty stores */
+    *i8p = 1;
+    /* full persist */
+    VALGRIND_PMC_DO_FLUSH(base, 8);
+    VALGRIND_PMC_DO_FENCE;
+    VALGRIND_PMC_DO_COMMIT;
+    VALGRIND_PMC_NO_LOG_STORES;
+    VALGRIND_PMC_DO_FENCE;
+    VALGRIND_PMC_ADD_LOG_REGION(i16p, 8);
+    *i16p = 2;
+    VALGRIND_PMC_DO_FLUSH(base+8, 8);
+    VALGRIND_PMC_DO_FENCE;
+    VALGRIND_PMC_DO_COMMIT;
+    *i32p = 3;
+    VALGRIND_PMC_DO_FLUSH(base+16, 8);
+    i32p += 8;
+    *i32p = 3;
+    VALGRIND_PMC_FULL_REORDER;
+    VALGRIND_PMC_ONLY_FAULT;
+    VALGRIND_PMC_PARTIAL_REORDER;
+    VALGRIND_PMC_STOP_REORDER_FAULT;
+    return 0;
+}
