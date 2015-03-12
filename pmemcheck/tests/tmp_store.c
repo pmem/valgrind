c69b7831c1a27d4d97023c85c2162b28d4853e58
Tomasz Kapela <tomasz.kapela@intel.com>
2015-03-12 16:40:54 +0100

    pmemcheck: initial version of the pmemcheck tool

---
+++ pmemcheck/tests/tmp_store.c
@@ -0,0 +1,41 @@
+/*
+ * Persistent memory checker.
+ * Copyright (c) 2014-2015, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, or (at your option) any later version, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+#include "common.h"
+
+#define FILE_SIZE (16 * 1024 * 1024)
+
+int main ( void )
+{
+    /* make, map and register a temporary file */
+    void *base = make_map_tmpfile(FILE_SIZE);
+
+    int8_t i8 = 1;
+    int16_t i16 = 2;
+    int32_t i32 = 3;
+    int64_t i64 = 4;
+
+    int8_t *i8p = base;
+    int16_t *i16p = base+8;
+    int32_t *i32p = base+16;
+    int64_t *i64p = base+24;
+
+    /* dirty stores */
+    *i8p = i8;
+    *i16p = i16;
+    *i32p = i32;
+    *i64p = i64;
+
+    return 0;
+}
