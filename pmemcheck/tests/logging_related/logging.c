14fd76858385642eb8b6efb1f0d082e20662ed86
Tomasz Kapela <tomasz.kapela@intel.com>
2015-04-16 14:33:18 +0200

    pmemcheck: add support for forcing flush alignment
    According to issue #8 pmemcheck now has the option to force flush
    alignment. By default this feature is turned off, meaning all flushes
    registered through the macro will not be aligned to the native machine's
    cache line size.
    
    Closes #8

--- pmemcheck/tests/logging_related/logging.c
+++ pmemcheck/tests/logging_related/logging.c
@@ -13,6 +13,7 @@
  * more details.
  */
 #include "../common.h"
+#include <stdint.h>
 
 #define FILE_SIZE (16 * 1024 * 1024)
 
@@ -22,8 +23,8 @@ int main ( void )
     void *base = make_map_tmpfile(FILE_SIZE);
 
     int8_t *i8p = base;
-    int16_t *i16p = base+8;
-    int32_t *i32p = base+16;
+    int16_t *i16p = (int16_t *)((uintptr_t)base + 8);
+    int32_t *i32p = (int32_t *)((uintptr_t)base + 16);
 
     VALGRIND_PMC_LOG_STORES;
     /* dirty stores */
@@ -36,11 +37,11 @@ int main ( void )
     VALGRIND_PMC_DO_FENCE;
     VALGRIND_PMC_ADD_LOG_REGION(i16p, 8);
     *i16p = 2;
-    VALGRIND_PMC_DO_FLUSH(base+8, 8);
+    VALGRIND_PMC_DO_FLUSH(i16p, 8);
     VALGRIND_PMC_DO_FENCE;
     VALGRIND_PMC_DO_COMMIT;
     *i32p = 3;
-    VALGRIND_PMC_DO_FLUSH(base+16, 8);
+    VALGRIND_PMC_DO_FLUSH(i32p, 8);
     i32p += 8;
     *i32p = 3;
     VALGRIND_PMC_FULL_REORDER;
