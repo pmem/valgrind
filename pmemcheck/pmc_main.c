860997426fd53f251b587749c30bf32f5b1f465a
Weronika Lewandowska <weronika.lewandowska@intel.com>
2018-08-22 13:03:36 +0200

    pmemcheck: bump version from 0.2 to 1.0

--- pmemcheck/pmc_main.c
+++ pmemcheck/pmc_main.c
@@ -1920,7 +1920,7 @@ static void
 pmc_pre_clo_init(void)
 {
     VG_(details_name)("pmemcheck");
-    VG_(details_version)("0.2");
+    VG_(details_version)("1.0");
     VG_(details_description)("a simple persistent store checker");
     VG_(details_copyright_author)("Copyright (c) 2014-2016, Intel Corporation");
     VG_(details_bug_reports_to)("tomasz.kapela@intel.com");
